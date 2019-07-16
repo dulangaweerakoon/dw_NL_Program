36dc80e6c400d2db838e6e7531e45bbb6154125e
larcuser <larcuser@Dulanga-MacBook-Pro-13.local>
2019-07-16 15:23:37 +0800

    CMU Dataset Added

--- robot_reasoning.py
+++ robot_reasoning.py
@@ -6,6 +6,8 @@ from scipy.cluster.hierarchy import cophenet
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import euclidean
 configs = []
+COLOURS = {"blue":"b","green":"g","yellow":"y","red":"r","orange":"r","brown":"r"}
+subscenes = {}
 with open('All_blocklocations.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     i = -1;
@@ -16,6 +18,7 @@ with open('All_blocklocations.csv') as csv_file:
         configs.append([row[5],float(row[1]),float(row[2])])
 
 batch = configs[0:15]
+print(batch)
 X = [x[1:] for x in batch]
 labels = [x[0] for x in batch]
 
@@ -36,5 +39,45 @@ def get_linkage(id):
 
 
 def break_program_to_subprograms(program):
-    
+    subprograms = []
+    program = program.split(' ')
+    subs = []
+    for pr in program:
+        if "arg" in pr:
+            subs.append(pr)
+            subprograms.append(subs)
+            subs = []
+        elif "ans" in pr:
+            print("df`")
+            subs.append(pr)
+            subprograms.append(subs)
+            subs = []
+        else:
+            subs.append(pr)
+    return subprograms
+def find_clusters(colour,num,subscene):
+    #print(colour,num)
+    outscene = []
+    if (subscene == None):
+        if (num==1):
+            print(num,"sdsd")
+            for x in labels:
+                if colour in x:
+                    outscene.append(1)
+                else:
+                    outscene.append(0)
+    return outscene   
+def execute_subprogram(subprogram,subscene):
+    if (subprogram[0]=="clusterof"):
+        if (subprogram[1]=="all"):
+            return find_clusters(COLOURS[subprogram[2]],int(subprogram[3]),None)
+        elif "arg" in subprogram[1]:
+            return find_clusters(COLOURS[subprogram[2]],int(subprogram[3]),subscenes[subprogram[1]])
+        
+        
 
+prg = break_program_to_subprograms("clusterof all green 1 arg1 clusterof yellow 1 ans")
+print(prg)                
+ans = execute_subprogram(prg[0],None)
+print(labels)
+print(ans)
